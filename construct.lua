6776637b2d0c36416225c46c784cfc96356adedb
Tom Leese <leese.thomas81@gmail.com>
2011-10-27 17:51:36 +0100

    Removed build script

--- construct.lua
+++
@@ -1,6 +0,0 @@
-Project("AllegroTemplate", "C++")
-
-Set("Packages", { "allegro", "allegro_image", "allegro_font", "allegro_ttf", "allegro_audio", "allegro_acodec" })
-Set("Sources", { "main.cpp", "game.cpp", "sectionmanager.cpp", "texturemanager.cpp", "fontmanager.cpp", "audiomanager.cpp" })
-
-Set("Output", "AllegroTemplate")
